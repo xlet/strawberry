5d315c91b64a583896f18620e77b809e59cebb83
Jackie Han <hanwwly@gmail.com>
2014-07-02 11:37:34 +0000

    add web module update mongo dao to spring config update conf to file properties

---
+++ web/src/main/java/cn/w/im/web/mongo/MongoTempMember.java
@@ -0,0 +1,93 @@
+package cn.w.im.web.mongo;
+
+import cn.w.im.domains.mongo.MongoDomain;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Id;
+
+/**
+ * @author jackie.
+ *         temp member.
+ */
+public class MongoTempMember implements MongoDomain {
+
+    @Id
+    private ObjectId id;
+    private long persistentDate;
+    private String name;
+    private String nickname;
+    private String source;
+
+    @Override
+    public ObjectId getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    @Override
+    public long getPersistentDate() {
+        return this.persistentDate;
+    }
+
+    @Override
+    public void setPersistentDate(long persistentDate) {
+        this.persistentDate = persistentDate;
+    }
+
+    /**
+     * get name.
+     *
+     * @return name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * set name.
+     *
+     * @param name name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * get nickname.
+     *
+     * @return nickname.
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * set nickname.
+     *
+     * @param nickname nickname.
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * get source.
+     *
+     * @return source.
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * set source.
+     *
+     * @param source source.
+     */
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
