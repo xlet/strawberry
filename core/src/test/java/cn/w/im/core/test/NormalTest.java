60bca8e7f4f2f545c6894810de95113ac841549f
Jackie Han <hanwwly@gmail.com>
2014-11-18 09:46:14 +0800

    add get lan ip address test

--- core/src/test/java/cn/w/im/core/test/NormalTest.java
+++ core/src/test/java/cn/w/im/core/test/NormalTest.java
@@ -3,7 +3,12 @@ package cn.w.im.core.test;
 import cn.w.im.core.MessageClientType;
 import org.junit.Test;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import static org.assertj.core.api.Assertions.*;
@@ -23,5 +28,43 @@ public class NormalTest {
 
         assertThat(clientTypes.contains(MessageClientType.Mobile)).isEqualTo(true);
 
+    }
+
+    @Test
+    public void test_get_local_ip_address() throws UnknownHostException {
+        InetAddress localAddress = this.getLocalHostLANAddress();
+        assertThat(localAddress.toString()).isEqualTo("10.0.40.38");
+    }
+
+    private InetAddress getLocalHostLANAddress() throws UnknownHostException {
+        try {
+            InetAddress candidateAddress = null;
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                for (Enumeration inetAddresses = iface.getInetAddresses(); inetAddresses.hasMoreElements(); ) {
+                    InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        if (inetAddress.isSiteLocalAddress()) {
+                            return inetAddress;
+                        } else if (candidateAddress == null) {
+                            candidateAddress = inetAddress;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            UnknownHostException unknownHostException = new UnknownHostException("Failed to determine LAN address:" + e);
+            unknownHostException.initCause(e);
+            throw unknownHostException;
+        }
     }
 }
--- core/src/test/java/cn/w/im/core/test/server/BusServerStartTester.java
+++ core/src/test/java/cn/w/im/core/test/server/BusServerStartTester.java
@@ -17,3 +17,4 @@ public class BusServerStartTester {
         server.start();
     }
 }
+
